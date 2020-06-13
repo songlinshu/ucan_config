978cd59c8b33325ee8bcd108113d1a6a7549e7eb
Marcin Janczara <marcin@mydevlog.cc>
2020-06-13 22:34:45 +0200

    Sending data using safe wrapper around the native libusb library

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,30 @@
 extern crate clap;
-use clap::{App, Arg, SubCommand};
-
-fn cli_interface() {
-     let matches = App::new("ucan_config")
-          .version("1.0")
-          .author("https://ucandevices.github.io/")
-          .about("Configuration for CAN devices")
-          .arg(Arg::with_name("protocol")
-               .short("p")
-               .long("protocol")
-               .help("Device protocol")
-               .value_name("usb|virtual")
-               .takes_value(true))
-          .arg(Arg::with_name("id")
-               .short("i")
-               .long("id")
-               .help("Device hardware ID")
-               .value_name("ex. 0x01234")
-               .takes_value(true))
-          .arg(Arg::with_name("name")
-               .help("Device name on uCAN network ex. can0 vcan0 ...")
-               .short("n")
-               .long("name")
-               .takes_value(true))
-          .arg(Arg::with_name("baundrate")
-               .help("CAN baudrate in kBPS ex 1M 100k 100000")
-               .short("b")
-               .long("baudrate")
-               .takes_value(true))
-          .arg(Arg::with_name("mode")
-               .help("CAN mode")
-               .short("m")
-               .required(false)
-               .value_name("loopback|normal|monitor")
-               .long("mode")
-               .takes_value(true))
-          .arg(Arg::with_name("config_file_path")
-               .help("Path with config file")
-               .short("c")
-               .required(false)
-               .long("config")
-               .takes_value(true))
-          .arg(Arg::with_name("v")
-               .short("v")
-               .multiple(true)
-               .help("Sets the level of verbosity"))
-          .arg(Arg::with_name("s")
-               .short("s")
-               .long("state")
-               .multiple(true)
-               .value_name("open|close")
-               .help("Set device state"))               
-          .get_matches();
-
-     // Gets a value for config if supplied by user, or defaults to "default.conf"
-     let protocol = matches.value_of("protocol").unwrap_or("default.conf");
-     println!("Value for protocol: {}", protocol);
-
-     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-     // required we could have used an 'if let' to conditionally get the value)
-     println!("Using name: {}", matches.value_of("name").unwrap());
-
-     // Vary the output based on how many times the user used the "verbose" flag
-     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-     match matches.occurrences_of("v") {
-          0 => println!("No verbose info"),
-          1 => println!("Some verbose info"),
-          2 => println!("Tons of verbose info"),
-          3 | _ => println!("Don't be crazy"),
-     }
-}
+extern crate libusb;
+use std::time::Duration;
 
 fn main() {
-     
-     cli_interface();
 
-     // more program logic goes here...
-     let context = zmq::Context::new();
-     let responder = context.socket(zmq::REP).unwrap();
- 
-     assert!(responder.bind("tcp://*:5555").is_ok());
+    let context = libusb::Context::new().unwrap();
+
+    for device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        // if device_desc.product_id() == 0x775
+        if device_desc.product_id() == 14155
+        {
+            let handle = context.open_device_with_vid_pid(device_desc.vendor_id(), device_desc.product_id());
+            println!("{:?}", handle.is_some());
+
+            let mut dev_handle = handle.unwrap();
+            let ret_claim = dev_handle.claim_interface(0);
+
+            let data_to_send : &[u8] = b"hello";
+            println!("{:?}\n{:?}", ret_claim, data_to_send);
+
+            let timeout = Duration::new(2, 0);
+            let ret_bulk = dev_handle.write_bulk(0x01, data_to_send, timeout);
+
+            print!("{:?}\n", ret_bulk);
+        }
+    }
 }
